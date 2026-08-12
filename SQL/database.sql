cc3721ae3cb1b1739c6622339f2783bf5aa80365
Jsangha3780 <102311352+Jsangha3780@users.noreply.github.com>
2026-08-11 23:19:36 -0400

    Added password field to users table

--- SQL/database.sql
+++ SQL/database.sql
@@ -8,6 +8,7 @@ CREATE TABLE colleges (
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
     college_id INT,
     last_name VARCHAR(255),
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
